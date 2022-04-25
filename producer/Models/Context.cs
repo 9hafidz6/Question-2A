24c5845a087b073af9bf315b0f7f179d6b710bd9
Hafidz <9hafidz6@gmail.com>
2022-04-25 09:41:18 +0800

    -

---
+++ producer/Models/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace producer.Models
+{
+    public class BookingContext : DbContext
+    {
+        public BookingContext(DbContextOptions<BookingContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<Credentials> Bookings { get; set; } = null!;
+    }
+}
---
+++ producer/Models/Credentials.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace producer.Models
+{
+    public class Credentials
+    {
+        public String Email { get; set; }
+        public String Password { get; set; }
+        public string Task { get; set; }
+    }
+}
